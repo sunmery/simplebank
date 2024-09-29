c611576b1892f78053e2d5680b78aa2357651208
Mandala <xiconz@qq.com>
2024-09-29 13:53:44 +0800

    test: test argocd

---
+++ deploy/frontend/kubernetes/argocd/create.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+# 启用 POSIX 模式并设置严格的错误处理机制
+set -o posix errexit -o pipefail
+
+PROJECT="simplebank"
+CLUSTER_URL="https://116.213.43.175:6443"
+REPO_URL="https://github.com/sunmery/simplebank.git"
+APPLICATION_NAME="bank-frontend1"
+NAMESPACE="ban1"
+
+# 创建项目
+argocd proj create ${PROJECT}
+
+# 删除
+# argocd proj delete frontend
+
+# 添加仓库到项目
+#  argocd proj add-source <PROJECT> <REPO>
+argocd proj add-source ${PROJECT} ${REPO_URL}
+
+# 删除
+# argocd proj remove-source <PROJECT> <REPO>
+
+# 排除项目
+# argocd proj add-source <PROJECT> !<REPO>
+
+# 添加集群与命名空间
+# argocd proj add-destination <PROJECT> <CLUSTER>,<NAMESPACE>
+# argocd proj remove-destination <PROJECT> <CLUSTER>,<NAMESPACE>
+argocd proj add-destination ${PROJECT} ${CLUSTER_URL} ${PROJECT}
+
+# 创建仓库秘钥
+#cat > gitlab-secret.yml <<EOF
+#apiVersion: v1
+#kind: Secret
+#metadata:
+#  name: argocd-example-apps
+#  labels:
+#    argocd.argoproj.io/secret-type: repository
+#type: Opaque
+#stringData:
+#  # Project scoped
+#  project: my-project1
+#  name: argocd-example-apps
+#  url: https://github.com/argoproj/argocd-example-apps.git
+#  username: ****
+#  password: ****
+#EOF
+
+# 创建APP
+argocd app create ${APPLICATION_NAME} \
+--project ${PROJECT} \
+--repo ${REPO_URL} \
+--path deploy/frontend/kubernetes/argocd \
+--dest-server ${CLUSTER_URL} \
+--dest-namespace ${NAMESPACE} \
+--validate
+
+# 查询该应用信息
+argocd app get ${APPLICATION_NAME}
+
+# 设置该应用为某个proj
+#argocd app set ${APPLICATION_NAME} --project ${PROJECT}
+
+# 删除
+#argocd app delete ${PROJECT}
+
+# 列出用户
+#argocd account list
--- deploy/frontend/kubernetes/argocd/init.sh
+++ deploy/frontend/kubernetes/argocd/init.sh
@@ -8,8 +8,6 @@ unset ROLE_NAME
 unset BRANCH
 unset PROJECT_GIT_URL
 
-# argocd所在的的命名空间
-export ARGOCD_NAMESPACE="argocd"
 # 角色名称, 用于管理项目
 export ROLE_NAME="admin"
 # Kubernetes集群地址
@@ -19,21 +17,18 @@ export CLUSTER_SERVER="https://116.213.43.175:6443"
 # 仓库URL
 export BRANCH="main"
 # 仓库地址
-export PROJECT_GIT_URL="https://github.com/sunmery/simple_bank.git"
+export PROJECT_GIT_URL="https://github.com/sunmery/simplebank.git"
 
 # Kubernetes 资源清单在仓库中的路径, 相对于仓库根目录的路径
-export FRONTEND_DEPLOY_PATH="deploy/frontend/kubernetes/argocd"
+export DEPLOY_PATH="deploy/frontend/kubernetes/argocd"
 
-# 前端命名空间, 不需要额外创建命名空间选择default即可
-export FRONTEND_NAMESPACE="bank"
-# argocd中的前端项目名, 用于分配团队人员的操作权限
-export FRONTEND_PROJECT="bank"
+# 前端项目的命名空间, 不需要额外创建命名空间选择default即可
+export NAMESPACE="bank1"
 
-# 前端项目
-export FRONTEND_APPLICATION_NAME="bank"
+export PROJECT="bank1"
 
-# 前端应用的名称
-export APPLICATION_NAME="bank-frontend"
+# argocd中的前端应用名, 用于分配团队人员的权限(查询/创建/同步/删除/操作)
+export APPLICATION_NAME="bank-frontend1"
 
 # 获取Git Repo URL
 if [ -z "$BRANCH" ]; then
@@ -59,15 +54,15 @@ cat > deploy/frontend/kubernetes/argocd/frontend-proj.yml <<EOF
 apiVersion: argoproj.io/v1alpha1
 kind: AppProject
 metadata:
-  name: ${FRONTEND_NAMESPACE} # 项目名称
-  namespace: ${ARGOCD_NAMESPACE} # 项目所在的命名空间，默认为argocd，可根据实际情况调整
+  name: ${NAMESPACE} # 项目名称
+  namespace: ${NAMESPACE} # 项目所在的命名空间，默认为argocd，可根据实际情况调整
 spec:
   # 项目描述
   description: "This project is for managing FRONTEND applications"
 
   # 允许应用部署到的命名空间列表
   destinations:
-  - namespace: ${FRONTEND_NAMESPACE}
+  - namespace: ${NAMESPACE}
     server: ${CLUSTER_SERVER} # 集群API地址，示例值需替换为实际地址
 
   # 允许的目标 K8s 资源类型
@@ -116,13 +111,13 @@ spec:
     # 注意这里的subjects配置应当符合Argo CD的RBAC规范，例如使用proj:frontend:admin
     # 定义角色权限
     policies:
-    # 允许 ROLE_NAME 角色对该命名空间下的项目进行: 获取/创建/同步/删除/操作
-    - p, proj:${FRONTEND_PROJECT}:${ROLE_NAME}, applications, get, ${APPLICATION_NAME}/*, allow
-    - p, proj:${FRONTEND_PROJECT}:${ROLE_NAME}, applications, create, ${APPLICATION_NAME}/*, allow
-    - p, proj:${FRONTEND_PROJECT}:${ROLE_NAME}, applications, sync, ${APPLICATION_NAME}/*, allow
-    - p, proj:${FRONTEND_PROJECT}:${ROLE_NAME}, applications, delete, ${APPLICATION_NAME}/*, allow
+    # 允许 ROLE_NAME 角色对该命名空间下的项目进行: 查询/创建/同步/删除/操作
+    - p, proj:${PROJECT}:${ROLE_NAME}, applications, get, ${APPLICATION_NAME}/*, allow
+    - p, proj:${PROJECT}:${ROLE_NAME}, applications, create, ${APPLICATION_NAME}/*, allow
+    - p, proj:${PROJECT}:${ROLE_NAME}, applications, sync, ${APPLICATION_NAME}/*, allow
+    - p, proj:${PROJECT}:${ROLE_NAME}, applications, delete, ${APPLICATION_NAME}/*, allow
     # 允许查看集群信息
-    - p, proj:${FRONTEND_PROJECT}:${ROLE_NAME}, clusters, get, ${FRONTEND_PROJECT_NAME}/*, allow
+    - p, proj:${PROJECT}:${ROLE_NAME}, clusters, get, ${PROJECT_NAME}/*, allow
   orphanedResources:
     warn: true
 # argocd proj create -f create-frontend-proj.yml
@@ -134,25 +129,25 @@ cat > deploy/frontend/kubernetes/argocd/frontend-app.yml <<EOF
 apiVersion: argoproj.io/v1alpha1  # 指定 Argo CD API 版本
 kind: Application  # 定义资源类型为 Application
 metadata: # 元数据部分
-  name: ${FRONTEND_APPLICATION_NAME}   # 指定 Application 的名称
-  namespace: ${ARGOCD_NAMESPACE}  # argocd所属的命名空间
+  name: ${APPLICATION_NAME}   # 指定 Application 的名称
+  namespace: ${NAMESPACE}  # 项目要部署到的命名空间
   # 定义资源的 finalizers
   # https://argo-cd.readthedocs.io/en/stable/user-guide/app_deletion/#about-the-deletion-finalizer
   finalizers:
     - resources-finalizer.argocd.argoproj.io  # 删除时行级联删除
     #- resources-finalizer.argocd.argoproj.io/background  # 删除时后台行级联删除
 spec: # 规范部分
-  project: ${FRONTEND_PROJECT_NAME}  # 应用程序将被配置的项目名称，这是在 Argo CD 中应用程序的一种组织方式
+  project: ${PROJECT}  # 应用程序将被配置的项目名称，这是在 Argo CD 中应用程序的一种组织方式
   source: # 指定源
     # Kubernetes 资源清单在仓库中的路径
-    path: ${FRONTEND_DEPLOY_PATH}
+    path: ${DEPLOY_PATH}
     # 指定 Git 仓库的 URL
     repoURL: ${PROJECT_GIT_URL}
     # 使用的 git 分支
     targetRevision: ${BRANCH}
   # 部署应用到Kubernetes 集群中的位置
   destination:
-    namespace: ${FRONTEND_NAMESPACE}  # 指定应用的命名空间
+    namespace: ${NAMESPACE}  # 指定应用的命名空间
     server: ${CLUSTER_SERVER}  # 如果部署到同一集群，可以省略
   syncPolicy: # 指定同步策略
     automated: # 自动化同步
